f1c89aebfd6906708a91659babedb078c01c306b
Roma <166724833+Romchik009@users.noreply.github.com>
2025-01-15 17:40:16 +0300

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,12 @@
+# Настройки окна и определение основных цветов
+display_horizontal_size_x = 800
+display_vertical_size_y = 600
+x1 = 400
+y1 = 400
+snake_size = 20
+game_speed = 10
+
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
